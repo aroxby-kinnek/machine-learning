9fd81da4fe7aba4e6a8d56006078ec7d45128196
Andy Roxby <aroxby@kinnek.com>
2018-09-20 17:17:31 -0400

    Spruce up main

--- main.py
+++ main.py
@@ -12,10 +12,19 @@ def main():
     """
     Test harness
     """
-    game_factory = lambda: Maze(Layout.from_string(Layout.EASY_STR))
+    def game_factory():
+        """
+        Creates the game we need
+        """
+        return Maze(Layout.from_string(Layout.EASY_STR))
+
     bot_factory = PlannedBot
     trainer = BotTrainer(game_factory, bot_factory, 4, 100, 2)
-    result = trainer.breed_best_bot()
+    generations, result = trainer.breed_best_bot()
+
+    msg = 'After {} generations, the bot {} the game'.format(
+        generations, 'won' if result.finished else 'lost')
+    print msg
     print 'Bot score: ', result.score
     print 'Bot plan: ', result.player.moves
 
