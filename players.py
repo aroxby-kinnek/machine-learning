828169270a3db5a2f80a991a1bb4648926ffe915
Andy Roxby <aroxby@kinnek.com>
2019-05-17 12:06:04 -0400

    Draft NEAT player

--- players.py
+++ players.py
@@ -60,3 +60,33 @@ class PlannedBot(BotPlayer):
         for _ in xrange(mutations):
             new_moves.append(random.choice(allowed_moves))
         return self.__class__(new_moves)
+
+
+class NeatBot(BotPlayer):
+    network_factory = GameNetwork()
+    def __init__(self, network):
+        self.games = {}
+        self.network = network or self.network_factory()
+
+    def next_move(self, game):
+        moves = self.network.eval_game(game)
+        # HACK
+        preference = 'wasd'
+        for move in preference:
+            if move in moves:
+                return move
+
+        # HACK
+        return 'Q'
+
+    def reproduce(self, allowed_moves, min_mutations, max_mutations):
+        mutations = random.randint(min_mutations, max_mutations)
+        new_network = self.network.deep_copy()
+        mutators = [    # FIXME: Add weights
+            new_network.add_random_neuron,
+            new_network.add_random_connection
+        ]
+        for _ in xrange(mutations):
+            mutator = random.choice(mutators)
+            mutator()
+        return self.__class__(new_network)
